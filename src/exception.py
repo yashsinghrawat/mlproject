f1bad1cf05598eec403bd4204a0afcb56eecc372
yashsinghrawat <yash.rawat282004@gmail.com>
2026-02-24 16:03:59 +0530

    logging and exception - updated exception

--- src/exception.py
+++ src/exception.py
@@ -1,5 +1,5 @@
 import sys
-import logging
+from src.logger import logging
 
 def error_message_detail(error,error_detail):
     _,_,exc_tb = error_detail.exc_info()
@@ -10,15 +10,21 @@ def error_message_detail(error,error_detail):
     return error_message
     
 
-class CustomExecption(Exception):
+class CustomException(Exception):
     def __init__(self, error_message,  error_detail):
         super().__init__(error_message)
-        self.error_message = error_message_detail(error_message, error_detail = error_detail)
+        self.error_message = error_message_detail(error_message, error_detail )
 
 # Inheriting
     def __str__(self):
 
         return self.error_message
     
-
-  
+if __name__ == "__main__":
+     try :
+            a = 1/0
+    
+        
+     except Exception as e:
+            logging.info("Division by zero" )
+            raise CustomException(e,sys)
